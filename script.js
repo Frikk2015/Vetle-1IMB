86e94a3c6b3ebf13dd59f5a411bce5e757761315
Vetle Fongen <93137521+Frikk2015@users.noreply.github.com>
2023-05-22 13:22:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,25 @@ Array.from(document.getElementsByClassName("nettside"))
         item.onmouseover = () => {
             container.dataset.activeIndex = index;
         }
-    });
+});
+
+let links = document.getElementsByClassName("nettside")
+
+
+for (i = 0; i < links.length; i++){
+    let number = 100 / links.length * i
+    let styles = `
+        #container[data-active-index="` + i + `"] > #bakgrunn {
+            background-position: 0% -` + number + `%;
+    }`
+    let styleSheet = document.createElement("style")
+    styleSheet.innerText = styles
+
+    let number2 = 20 / links.length * i + 30
+    let styles2 = `
+        #container[data-active-index="` + i + `"] > #bakgrunnBilde {
+            background-position: center ` + number2 + `%;
+    }`
+    styleSheet.innerText += styles2
+    document.head.appendChild(styleSheet)
+}
